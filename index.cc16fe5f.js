fee318b8e9f47c3f1d3040ef325f0530dee402e0
danretegan <96877914+danretegan@users.noreply.github.com>
2024-04-21 12:59:43 +0000

    Deploying to gh-pages from @ danretegan/modern-javascript@00c2bb7be444d840b0062ae8c5f1c0d218623761 🚀

---
+++ index.cc16fe5f.js
@@ -0,0 +1,3 @@
+//! Math object:
+console.log(Math),console.log(Math.PI);var random=Math.random();console.log("random number:",random);var random10=10*random;console.log("random number * 10 =",random10),console.log("round:",Math.round(random10)),console.log("floor:",Math.floor(random10)),console.log("ceil:",Math.ceil(random10)),console.log("trunc:",Math.trunc(random10)),console.log("un numar intreg, aleator, intre 1 si 100:",Math.round(100*random));
+//# sourceMappingURL=index.cc16fe5f.js.map
--- index.decb30e0.js
+++
@@ -1,3 +0,0 @@
-//! Objects in Arrays:
-var user={name:"Crystal",age:30,email:"crystal@thenetninja.co.uk",location:"Berlin",blogs:[{title:"why mac & cheese rules",likes:30},{title:"10 things to make with marmite",likes:50}],login:function(){},logout:function(){},logBlogs:function(){console.log(this),console.log(this.blogs),console.log("".concat(this.name," has written the following blogs:")),this.blogs.forEach((function(o){console.info("-",o.title,"/ likes:",o.likes)}))}};user.logBlogs();
-//# sourceMappingURL=index.decb30e0.js.map
