468f87519561eba0ec276b3c08eda1764dc1662e
danretegan <96877914+danretegan@users.noreply.github.com>
2024-04-21 12:58:31 +0000

    Deploying to gh-pages from @ danretegan/modern-javascript@bf47302ec314da234c87a90ba7c8f717244897aa 🚀

--- index.e85acd3d.js
+++
@@ -1,3 +0,0 @@
-//! object literals
-var user={name:"Crystal",age:30,email:"crystal@thenetninja.co.uk",location:"Berlin",blogs:["why mac & cheese rules","10 things to make with marmite"],login:function(){console.log("the user logged in")},logout:function(){console.log("the user logged out")},logBlogs:function(){console.log(this),console.log(this.blogs),console.log("".concat(this.name," has written the following blogs:")),this.blogs.forEach((function(o){console.info("-",o)}))}};user.logBlogs(),console.log(this);
-//# sourceMappingURL=index.e85acd3d.js.map
