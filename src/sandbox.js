1a1903d5a869996e7ed3c7b3f3e81e4de2819b60
Dan Retegan <danretegan@yahoo.com>
2024-04-21 17:58:35 +0300

    Other Ways to Query the DOM

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,36 +1,42 @@
-//TODO The Query Selector:
+//TODO get element by id:
 
-const paragraph1 = document.querySelector('p');
-const paragraph3 = document.querySelector('.error');
-const div2 = document.querySelector('div.error');
-
-//TODO Select element /Inspect /Copy /Copy selector!
-const paragraph2 = document.querySelector(
-  'body > div:nth-child(3) > p:nth-child(2)'
-);
-console.log(paragraph1);
-console.log(paragraph3);
-console.log(div2);
-console.log(paragraph2);
+const title = document.getElementById('page-title');
+console.log(title);
 console.log('=====================');
 
-//TODO Se selecteaza (si se listeaza) toate elementele <p>:
-const paragraphs = document.querySelectorAll('p');
+//TODO get element`s!` by class name (HTMLCollection):
+
+const errors = document.getElementsByClassName('error');
+console.log(errors);
+console.log(errors[0]);
+console.log(errors[1]);
+//! Atentie: metoda forEach NU functioneaza pe HTMLCollection!
+console.log('=====================');
+
+//TODO get element`s!` by their tag name (HTMLCollection):
+
+const paragraphs = document.getElementsByTagName('p');
 console.log(paragraphs);
-console.log('=====================');
-
-//TODO Sau selectam si listam un anumit element <p>:
 console.log(paragraphs[0]);
 console.log(paragraphs[1]);
 console.log(paragraphs[2]);
 console.log('=====================');
 
-//TODO Putem folosi metoda forEach:
-paragraphs.forEach(element => {
-  console.log(element);
-});
+//TODO Rezumat:
+console.log('HTMLCollection methods: .item() .namedItem()');
+console.log(
+  'Node List methods: .item() .entries() .forEach() .keys() .values()'
+);
 
-//TODO Selectam toate clasele .error:
-const errors = document.querySelectorAll('.error');
-console.log('=====================');
-console.log(errors);
+/**
+HTMLCollection methods:
+.item()
+.namedItem()
+
+Node List methods:
+.item()
+.entries()
+.forEach()
+.keys()
+.values() 
+*/
