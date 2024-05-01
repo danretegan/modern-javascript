938d2de28c2ee66f87f17cd4021361e394b8db3c
Dan Retegan <danretegan@yahoo.com>
2024-05-01 17:08:49 +0300

    Getting & Setting Attributes

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,32 +1,11 @@
-//! Adding & Changing Page Content
+const link = document.querySelector('a');
 
-const para = document.querySelector('p');
-console.log(para);
-console.log('vechiul text: ', para.innerText);
+console.log(link.getAttribute('href'));
+link.setAttribute('href', 'https://www.thenetninja.co.uk.com');
+link.innerText = 'The Net Ninja Website';
 
-//! Folosindu-ne de querySelector si de proprietatea innerText am INLOCUIT vechiul text in primul <p>:
-para.innerText = 'am schimbat textul in browser!';
-console.log('noul text:', para.innerText);
-console.log('====================');
+const mssg = document.querySelector('p');
 
-const paras = document.querySelectorAll('p');
-
-paras.forEach(element => {
-  console.log(element.innerText);
-  //! Folosindu-ne de querySelectorAll, de proprietatea innerText si de += am ADAUGAT text de data aceasta, in toate <p>:
-  element.innerText += ' new text';
-});
-console.log('====================');
-
-//! Inlocuim sau adaugam (+=) cod HTML:
-const content = document.querySelector('.content');
-console.log('vechiul HTML: ', content.innerHTML);
-content.innerHTML = '<h2> THIS IS A NEW h2 TITLE </h2>';
-console.log('noul HTML:', content.innerHTML);
-
-//! Adaugam (+=) in div-ul cu clasa content o lista de persoane, fiecare persoana intr-un paragraf:
-const people = ['Dan', 'Radu', 'Ion', 'Vasile'];
-
-people.forEach(element => {
-  content.innerHTML += `<p>${element}</p>`;
-});
+console.log(mssg.getAttribute('class'));
+mssg.setAttribute('class', 'succes');
+mssg.setAttribute('style', 'color:red;');
