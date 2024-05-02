9ea88d94db36583af127141879102f50a04bd96b
Dan Retegan <danretegan@yahoo.com>
2024-05-02 06:14:19 +0300

    Update sandbox.js

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,11 +1,17 @@
 const link = document.querySelector('a');
 
 console.log(link.getAttribute('href'));
-link.setAttribute('href', 'https://www.thenetninja.co.uk.com');
-link.innerText = 'The Net Ninja Website';
+
+link.setAttribute('href', 'https://www.youtube.com');
+link.innerText = 'change to YouTube!';
+
+console.log(link.getAttribute('href'));
+
+console.log('=======================');
 
 const mssg = document.querySelector('p');
 
 console.log(mssg.getAttribute('class'));
-mssg.setAttribute('class', 'succes');
-mssg.setAttribute('style', 'color:red;');
+mssg.setAttribute('class', 'success');
+console.log(mssg.getAttribute('class'));
+mssg.setAttribute('style', 'color: green;');
