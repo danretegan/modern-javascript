3d1a3f0781b4711ebaf88b46cb7918f5ea91fd6a
danretegan <96877914+danretegan@users.noreply.github.com>
2024-05-02 03:39:44 +0000

    Deploying to gh-pages from @ danretegan/modern-javascript@d9a25f1bdfb69fa271c5fc522c792ab7bcb1610c 🚀

---
+++ index.4513af5e.js
@@ -0,0 +1,2 @@
+var title=document.querySelector("h1");console.log(title.style),console.log(title.style.color),title.style.margin="50px",title.style.color="green",title.style.fontSize="16px",title.style.margin="";
+//# sourceMappingURL=index.4513af5e.js.map
--- index.9d95f61c.js
+++
@@ -1,2 +0,0 @@
-var link=document.querySelector("a");console.log(link.getAttribute("href")),link.setAttribute("href","https://www.youtube.com"),link.innerText="change to YouTube!",console.log(link.getAttribute("href")),console.log("=======================");var mssg=document.querySelector("p");console.log(mssg.getAttribute("class")),mssg.setAttribute("class","success"),console.log(mssg.getAttribute("class")),mssg.setAttribute("style","color: green;");
-//# sourceMappingURL=index.9d95f61c.js.map
