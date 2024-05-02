b4bc7456b9518429a3040b0fb584347042ceca7e
danretegan <96877914+danretegan@users.noreply.github.com>
2024-05-02 03:14:55 +0000

    Deploying to gh-pages from @ danretegan/modern-javascript@b3440a3fc9a80e40269f18c5baf2d5bf86154b8b 🚀

--- index.2585894e.js
+++
@@ -1,2 +0,0 @@
-var link=document.querySelector("a");console.log(link.getAttribute("href")),link.setAttribute("href","https://www.thenetninja.co.uk.com"),link.innerText="The Net Ninja Website";var mssg=document.querySelector("p");console.log(mssg.getAttribute("class")),mssg.setAttribute("class","succes"),mssg.setAttribute("style","color:red;");
-//# sourceMappingURL=index.2585894e.js.map
