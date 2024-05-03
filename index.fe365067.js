5843804ac16d86ea5e4cf0edae67a401c3226c5d
danretegan <96877914+danretegan@users.noreply.github.com>
2024-05-03 06:27:22 +0000

    Deploying to gh-pages from @ danretegan/modern-javascript@f28f349820f4ff9f7af21888c8c7779f8d50ca25 🚀

---
+++ index.fe365067.js
@@ -0,0 +1,2 @@
+var copy=document.querySelector(".copy-me");copy.addEventListener("copy",(function(){console.log("© The document is under copyright!")}));var box=document.querySelector(".box");box.addEventListener("mousemove",(function(e){box.textContent="X pos: ".concat(e.offsetX,", Y pos: ").concat(e.offsetY)})),document.addEventListener("wheel",(function(e){console.log(e.pageX,e.pageY)}));
+//# sourceMappingURL=index.fe365067.js.map
