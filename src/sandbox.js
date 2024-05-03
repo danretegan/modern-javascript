0ea5d7c8ebb4e2d89f1be12a6fef2ce6e6a7499b
Dan Retegan <danretegan@yahoo.com>
2024-05-03 11:41:19 +0300

    Revert "Testing RegEx Patterns"
    This reverts commit 4ec152b334c66f6a864c44b27a2d0ec19ed85fce.

--- src/sandbox.js
+++ src/sandbox.js
@@ -7,20 +7,3 @@ form.addEventListener('submit', e => {
   //TODO daca input-ul are id sau name putem folosi .notation si id /name:
   console.log(form.username.value);
 });
-
-//TODO testing RegEx:
-const username = 'shaunp';
-const pattern = /^[a-z]{6,}$/;
-
-//TODO Metoda 1 test:
-let result = pattern.test(username);
-
-if (result) {
-  console.log('Metoda 1 test: RegEx test pased! :)');
-} else {
-  console.log('Metoda 1 test: RegEx test failed! :(');
-}
-
-//TODO Metoda 2 search:
-let result2 = username.search(pattern);
-console.log('Metoda 2 search:', result2);
