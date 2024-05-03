84855f9e7b8c3c127c21ffcbc739cba9fd3dcaae
danretegan <96877914+danretegan@users.noreply.github.com>
2024-05-03 10:16:03 +0000

    Deploying to gh-pages from @ danretegan/modern-javascript@2e8ca2be24d6da0e6bc126a7f70b555d8318b2a3 🚀

--- index.195f49a8.js
+++
@@ -1,2 +0,0 @@
-var form=document.querySelector(".signup-form"),feedback=document.querySelector(".feedback");form.addEventListener("submit",(function(e){e.preventDefault();var t=form.username.value;/^[a-zA-Z]{6,12}$/.test(t)?feedback.textContent="that username is valid!":feedback.textContent="username must contain only letters & be between 6 & 12 characters"}));
-//# sourceMappingURL=index.195f49a8.js.map
