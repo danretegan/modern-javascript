4f88c896f46ac90d7197362e9124055e6ff91b34
danretegan <96877914+danretegan@users.noreply.github.com>
2024-05-03 08:00:34 +0000

    Deploying to gh-pages from @ danretegan/modern-javascript@25aa8923557ea93068f452171b3d6f9c6477a96d 🚀

---
+++ index.719b3edb.js
@@ -0,0 +1,2 @@
+var form=document.querySelector(".signup-form");form.addEventListener("submit",(function(e){e.preventDefault(),console.log(form.username.value)}));
+//# sourceMappingURL=index.719b3edb.js.map
