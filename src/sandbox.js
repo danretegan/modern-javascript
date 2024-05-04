6680f4d6e5268a4a0a74fb03b38625b4ef855729
Dan Retegan <danretegan@yahoo.com>
2024-05-04 09:33:15 +0300

    Reduce Method
    array.reduce((previousValue, element, index, array) => {
      // Corpul funcției callback
    }, initialValue);

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,26 +1,12 @@
-// Map Method:
-const prices = [20, 10, 30, 25, 15, 40, 80, 5];
+//! Reduce Method:
+const scores = [10, 20, 60, 40, 70, 90, 30, 5];
 
-const salePrices = prices.map(pret => pret / 2);
-console.log('salePrices (50% off):', salePrices);
-
-console.log('===============================');
-
-const products = [
-  { name: 'gold star', price: 20 },
-  { name: 'mushroom', price: 40 },
-  { name: 'green shells', price: 30 },
-  { name: 'banana skin', price: 10 },
-  { name: 'red shells', price: 50 },
-];
-
-const saleProducts = products.map(element => {
-  if (element.price > 30) {
-    //TODO refacem elementul obiect cu pretul redus (nu modificam originalul!):
-    return { name: element.name, price: element.price / 2 };
+const reduceScores = scores.reduce((acc, element) => {
+  if (element > 50) {
+    acc = acc + 1;
+    // acc++
   }
-  //TODO returnam elementul obiect nemodificat:
-  return element;
-});
+  return acc;
+}, 0);
 
-console.log('some products on sale:', saleProducts);
+console.log(reduceScores);
