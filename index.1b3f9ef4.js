9eaed82d16b372042ba6e9813a3dd432cac6fdce
danretegan <96877914+danretegan@users.noreply.github.com>
2024-05-04 06:53:47 +0000

    Deploying to gh-pages from @ danretegan/modern-javascript@16675027682d4427b816436d608cb06c38fd974d 🚀

--- index.1b3f9ef4.js
+++
@@ -1,3 +0,0 @@
-//! Reduce Method:
-var scores=[10,20,60,40,70,90,30,5],reduceScores=scores.reduce((function(e,r){return r>50&&(e+=1),e}),0);console.log(reduceScores);
-//# sourceMappingURL=index.1b3f9ef4.js.map
