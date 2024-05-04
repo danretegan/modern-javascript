27a3f92b70a5bbae7ef66e49ad22160a37205a6d
danretegan <96877914+danretegan@users.noreply.github.com>
2024-05-04 08:18:36 +0000

    Deploying to gh-pages from @ danretegan/modern-javascript@eafbb037eaccfd866d0617db63b73236c8af0d70 🚀

--- index.5ac20341.js
+++
@@ -1,3 +0,0 @@
-//! Sort Method:
-var names=["mario","shaun","chun-li","yoshi","luigi"];console.log('array-ul original, "names":',names);var reverseArray=names.reverse();console.log(".reverse() inverseaza ordinea in array:",reverseArray),console.log('listam din nou array-ul "names":',names),console.warn("Dupa cum se vede mai sus, array-ul original a fost modificat! Deci nu are rost folosirea unei noi constante.");
-//# sourceMappingURL=index.5ac20341.js.map
