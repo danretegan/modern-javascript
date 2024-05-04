71d9deb2b691f1d617ea95ffcdb7fbde92cb55d8
Dan Retegan <danretegan@yahoo.com>
2024-05-04 08:14:50 +0300

    Filter Method

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,30 +1,18 @@
-const form = document.querySelector('.signup-form');
-const feedback = document.querySelector('.feedback');
-const usernamePattern = /^[a-zA-Z]{6,12}$/;
+const scores = [10, 30, 15, 25, 50, 40, 5];
 
-//TODO validation:
-form.addEventListener('submit', e => {
-  e.preventDefault();
-
-  const username = form.username.value;
-
-  if (usernamePattern.test(username)) {
-    feedback.textContent = 'that username is valid!';
-  } else {
-    feedback.textContent =
-      'username must contain only letters & be between 6 & 12 characters';
-  }
+const arrayFiltrat = scores.filter(elem => {
+  return elem > 25;
 });
 
-//TODO live feedback:
-form.username.addEventListener('keyup', e => {
-  // console.log(e.target.value, form.username.value);
-  console.log(e);
-  if (usernamePattern.test(e.target.value)) {
-    // console.info('passed!');
-    form.username.setAttribute('class', 'success');
-  } else {
-    // console.error('failed!');
-    form.username.setAttribute('class', 'error');
-  }
-});
+console.log('arrayFiltrat:', arrayFiltrat);
+
+const users = [
+  { name: 'shaun', premium: true },
+  { name: 'yoshi', premium: false },
+  { name: 'mario', premium: false },
+  { name: 'chun-li', premium: true },
+];
+
+const premiumUsers = users.filter(element => element.premium);
+
+console.log('premiumUsers:', premiumUsers);
