fd1f98bd2d7359fb8d22a85e63c21aa8b1580c3d
danretegan <96877914+danretegan@users.noreply.github.com>
2024-05-04 05:15:26 +0000

    Deploying to gh-pages from @ danretegan/modern-javascript@689ea0dcaa19dc4c47f5a3b66fff4c7e80c01403 🚀

---
+++ index.0a57b1ad.js
@@ -0,0 +1,2 @@
+var scores=[10,30,15,25,50,40,5],arrayFiltrat=scores.filter((function(r){return r>25}));console.log("arrayFiltrat:",arrayFiltrat);var users=[{name:"shaun",premium:!0},{name:"yoshi",premium:!1},{name:"mario",premium:!1},{name:"chun-li",premium:!0}],premiumUsers=users.filter((function(r){return r.premium}));console.log("premiumUsers:",premiumUsers);
+//# sourceMappingURL=index.0a57b1ad.js.map
