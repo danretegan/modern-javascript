307964ca750f24327b328f339c78a3abe2cebcac
Dan Retegan <danretegan@yahoo.com>
2024-05-04 10:06:37 +0300

    Find Method
    array.find((element, index, array) => {
      // Corpul funcției callback
    });
    - Nu modifică matricea inițială.
    - Iterează peste matricea inițială, element cu element.
    - Returnează primul element care îndeplinește condiția, adică atunci când callback-ul returnează true.
    - Dacă niciun element nu satisface condiția , adică callback-ul returnează false pentru toate elementele, metoda returnează undefined.

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,45 +1,8 @@
-//! Reduce Method:
-const scores1 = [10, 20, 60, 40, 70, 90, 30, 5];
+//! Find Method:
+const scores = [10, 5, 0, 40, 60, 10, 20, 70];
 
-const reduceScores = scores1.reduce((acc, element) => {
-  if (element > 50) {
-    acc = acc + 1;
-    // acc++
-  }
-  return acc;
-}, 0);
+const firstHighScore = scores.find(element => {
+  return element > 50;
+});
 
-console.warn(`Avem ${reduceScores} scoruri mai mari de 50!`);
-
-const scores2 = [
-  { player: 'mario', score: 50 },
-  { player: 'yoshi', score: 30 },
-  { player: 'mario', score: 70 },
-  { player: 'cryst', score: 60 },
-  { player: 'mario', score: 50 },
-  { player: 'yoshi', score: 30 },
-  { player: 'mario', score: 70 },
-  { player: 'cryst', score: 60 },
-  { player: 'mario', score: 50 },
-  { player: 'yoshi', score: 30 },
-  { player: 'mario', score: 70 },
-  { player: 'cryst', score: 60 },
-  { player: 'mario', score: 50 },
-  { player: 'yoshi', score: 30 },
-  { player: 'mario', score: 70 },
-  { player: 'cryst', score: 60 },
-  { player: 'mario', score: 50 },
-  { player: 'yoshi', score: 30 },
-  { player: 'mario', score: 70 },
-  { player: 'cryst', score: 60 },
-];
-
-const mariosScore = scores2.reduce((acc, element) => {
-  if (element.player === 'mario') {
-    acc = acc + element.score;
-    // acc += element.score;
-  }
-  return acc;
-}, 0);
-
-console.info(`Mario a facut in total ${mariosScore} puncte.`);
+console.log(`Primul scor mai mare decat 50 este: ${firstHighScore}`);
