ccba33d8af1854300383d75c264c07f038e5c74c
danretegan <96877914+danretegan@users.noreply.github.com>
2024-05-04 08:16:09 +0000

    Deploying to gh-pages from @ danretegan/modern-javascript@0f45fcaef2a4bc3df9fa80122f26d85303cd5f37 🚀

---
+++ index.5ac20341.js
@@ -0,0 +1,3 @@
+//! Sort Method:
+var names=["mario","shaun","chun-li","yoshi","luigi"];console.log('array-ul original, "names":',names);var reverseArray=names.reverse();console.log(".reverse() inverseaza ordinea in array:",reverseArray),console.log('listam din nou array-ul "names":',names),console.warn("Dupa cum se vede mai sus, array-ul original a fost modificat! Deci nu are rost folosirea unei noi constante.");
+//# sourceMappingURL=index.5ac20341.js.map
--- index.70cb3b13.js
+++
@@ -1,3 +0,0 @@
-//! Find Method:
-var scores=[10,5,0,40,60,10,20,70],firstHighScore=scores.find((function(r){return r>50}));console.log("Primul scor mai mare decat 50 este: ".concat(firstHighScore));
-//# sourceMappingURL=index.70cb3b13.js.map
