aff48b074009ea4ba2476d2cb86feac9077feb39
danretegan <96877914+danretegan@users.noreply.github.com>
2024-05-06 05:25:37 +0000

    Deploying to gh-pages from @ danretegan/modern-javascript@5665ed9de3df9a5db9196719a3291743b26eeadc 🚀

---
+++ index.49b78002.js
@@ -0,0 +1,10 @@
+var _typeof=function(o){return o&&"undefined"!=typeof Symbol&&o.constructor===Symbol?"symbol":typeof o},now=new Date;
+//! dates & times:
+console.log(now),console.warn("Date() este un",void 0===now?"undefined":_typeof(now)),
+//! year, month, day, times:
+console.log("getFullYear:",now.getFullYear()),console.warn("Atentie! getMonth:",now.getMonth()),console.log("getDate:",now.getDate()),console.warn("Atentie: in ce zi din saptamana suntem / getDay:",now.getDay()),console.log("getHours:",now.getHours()),console.log("getMinutes:",now.getMinutes()),console.log("getSeconds",now.getSeconds()),
+//! timestamps:
+console.log("timestamps in ms from 01.ian.1970 / getTime:",now.getTime()),
+//! date strings:
+console.log("toDateString:",now.toDateString()),console.log("toTimeString:",now.toTimeString()),console.log("toLocaleString:",now.toLocaleString());
+//# sourceMappingURL=index.49b78002.js.map
