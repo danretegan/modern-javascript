87d2afacd0d24f0f5bdfcb67ecebaed0ff82267e
Dan Retegan <danretegan@yahoo.com>
2024-05-06 17:47:05 +0300

    Callback Functions

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,13 +1,32 @@
-const request = new XMLHttpRequest();
+const getTodos = callback => {
+  const request = new XMLHttpRequest();
 
-request.addEventListener('readystatechange', () => {
-  if (request.readyState === 4 && request.status === 200) {
-    console.info('request:', request);
-    console.log('request.responseText:', request.responseText);
-  } else if (request.readyState === 4) {
-    console.error('Could not fetch the data!');
+  request.addEventListener('readystatechange', () => {
+    if (request.readyState === 4 && request.status === 200) {
+      callback(undefined, request.responseText);
+    } else if (request.readyState === 4) {
+      callback('Could not fetch data!', undefined);
+    }
+  });
+
+  request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
+  request.send();
+};
+
+//! Exemplificare cod asincron:
+console.warn('Exemplificare cod asincron:');
+console.log(1);
+console.log(2);
+
+getTodos((err, data) => {
+  console.warn('callback fired!');
+
+  if (err) {
+    console.log('error:', err);
+  } else {
+    console.log('data:', data);
   }
 });
 
-request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
-request.send();
+console.log(3);
+console.log(4);
