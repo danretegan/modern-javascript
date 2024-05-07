cf9ff79066d5a8a45a2e56804d7647eb324af0fa
danretegan <96877914+danretegan@users.noreply.github.com>
2024-05-07 06:01:23 +0000

    Deploying to gh-pages from @ danretegan/modern-javascript@bc163af102551abf69053734b430d5ae0b8f4b8c 🚀

---
+++ index.f566197d.js
@@ -0,0 +1,2 @@
+fetch("./todos/luigi.json").then((function(o){return console.log("resolved:",o),o.json()})).then((function(o){console.log(o)})).catch((function(o){console.log("rejected:",o)}));
+//# sourceMappingURL=index.f566197d.js.map
