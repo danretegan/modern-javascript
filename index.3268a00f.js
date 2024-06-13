9d71ae14f09dba401370710ccd07e2581cfd856b
danretegan <96877914+danretegan@users.noreply.github.com>
2024-06-13 10:49:37 +0000

    Deploying to gh-pages from @ danretegan/modern-javascript@0be6890eae3bdc56458f683227904ebbbaa873b6 🚀

--- index.3268a00f.js
+++ index.3268a00f.js
@@ -2,6 +2,6 @@
 localStorage.setItem("name","Mario"),localStorage.setItem("age",50);
 //! get data from localStorage:
 let e=localStorage.getItem("name"),a=localStorage.getItem("age");console.log(e,a),
-//! updating data from localStorage:
-localStorage.setItem("name","Luigi"),localStorage.age=40,e=localStorage.getItem("name"),a=localStorage.getItem("age"),console.log(e,a);
-//# sourceMappingURL=index.ba53b09f.js.map
+//! deleting data from local storage:
+localStorage.clear(),e=localStorage.getItem("name"),a=localStorage.getItem("age"),console.log(e,a);
+//# sourceMappingURL=index.3268a00f.js.map
--- index.57228c86.js
+++ index.57228c86.js
@@ -3,6 +3,6 @@
 localStorage.setItem("name","Mario"),localStorage.setItem("age",50);
 //! get data from localStorage:
 var e=localStorage.getItem("name"),a=localStorage.getItem("age");console.log(e,a),
-//! updating data from localStorage:
-localStorage.setItem("name","Luigi"),localStorage.age=40,e=localStorage.getItem("name"),a=localStorage.getItem("age"),console.log(e,a)}();
-//# sourceMappingURL=index.f7ddd6ea.js.map
+//! deleting data from local storage:
+localStorage.clear(),e=localStorage.getItem("name"),a=localStorage.getItem("age"),console.log(e,a)}();
+//# sourceMappingURL=index.57228c86.js.map
