543075af93ba560ac4f504301a884c6b555d09b6
Dan Retegan <danretegan@yahoo.com>
2024-06-14 12:20:47 +0300

    Class Methods & Method Chaining
    - return this;

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,16 +1,42 @@
 class User {
-  constructor(username, ceva) {
-    //! set up properties of the object:
+  constructor(username, email) {
+    //* set up properties of the object:
     this.username = username;
-    this.ceva = ceva;
+    this.email = email;
+    this.score = 0;
+  }
+  login() {
+    console.log(`${this.username} just logged in.`);
+
+    //* daca vrem sa inlantuim metodele (method chaining) trebuie sa returnam "this":
+    return this;
+  }
+  logout() {
+    console.log(`${this.username} logged out.`);
+
+    return this;
+  }
+  incScore() {
+    this.score = this.score + 1;
+
+    console.log(`${this.username} has a score of ${this.score}`);
+
+    return this;
   }
 }
 
-const userOne = new User('Dan', 'ceva');
-const userTwo = new User('Ion', 'ceva');
+const userOne = new User('danretegan', 'danretegan@yahoo.com');
+const userTwo = new User('ion', 'ion@ion.com');
 
 console.log(userOne);
+userOne.login();
+userOne.incScore();
+userOne.logout();
+
+console.log('======================');
+
 console.log(userTwo);
+userTwo.login().incScore().incScore().logout();
 
 //* the "new" keyword:
 //* 1. - it creates a new empty object {};
