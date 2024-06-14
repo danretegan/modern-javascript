39803b2aeaafedd9633d010704bae39bfd1c432c
danretegan <96877914+danretegan@users.noreply.github.com>
2024-06-14 16:59:59 +0000

    Deploying to gh-pages from @ danretegan/modern-javascript@64d2e9e8695d7176549c26ce127b4f029a68d9e5 🚀

--- index.9b59c9fe.js
+++
@@ -1,2 +0,0 @@
-localStorage.setItem("todos",JSON.stringify([{text:"play mariokart",author:"shaun"},{text:"buy some milk",author:"mario"},{text:"buy some bread",author:"luigi"}]));const t=localStorage.getItem("todos");console.log(JSON.parse(t));
-//# sourceMappingURL=index.9b59c9fe.js.map
