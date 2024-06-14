c7217e297a6114fe232960aa24b71024401497d7
Dan Retegan <danretegan@yahoo.com>
2024-06-14 18:07:57 +0300

    Prototype Model
    prototype

--- src/sandbox.js
+++ src/sandbox.js
@@ -3,17 +3,14 @@
 function User(username, email) {
   this.username = username;
   this.email = email;
-  this.login = function () {
-    console.log(`${this.username} has logged in.`);
-  };
 }
 
-// class User {
-//   constructor(username, email) {
-//     this.username = username;
-//     this.email = email;
-//   }
-// }
+//! definim metoda direct pe prototype:
+User.prototype.login = function () {
+  console.log(`${this.username} has logged in.`);
+
+  return this; //* allow method chaining!
+};
 
 const userOne = new User('danretegan', 'danretegan@yahoo.com');
 const userTwo = new User('ion', 'ion@ion.com');
@@ -21,7 +18,4 @@ const userTwo = new User('ion', 'ion@ion.com');
 console.log(userOne, userTwo);
 userOne.login();
 
-// the 'new' keyword
-// 1 - it creates a new empty object {}
-// 2 - it binds the value of 'this' to the new empty object
-// 3 - it calls the constructor function to 'build' the object
+console.log(User.prototype);
