d68031e41d1bf1565bd20822dd1bee258b6d70a8
danretegan <96877914+danretegan@users.noreply.github.com>
2024-06-19 05:32:36 +0000

    Deploying to gh-pages from @ danretegan/modern-javascript@8464a39757af710342c9ebf2f27d2ae21a5c848f 🚀

--- index.6a94189e.js
+++
@@ -1,6 +0,0 @@
-//! Constructorul User:
-function o(o,n){this.username=o,this.email=n}
-//! Definim metoda login pe prototipul User:
-//! Constructorul Admin:
-function n(n,e,t){o.call(this,n,e),this.title=t}o.prototype.login=function(){return console.log(`${this.username} has logged in.`),this},n.prototype=Object.create(o.prototype),n.prototype.deleteUser=function(){};const e=new o("danretegan","danretegan@yahoo.com"),t=new o("ion","ion@ion.com"),i=new n("admin","admin@admin.com","black-belt-ninja");console.log(e,t,i),e.login(),console.log(n.prototype);
-//# sourceMappingURL=index.6a94189e.js.map
--- index.bc5aa24c.js
+++
@@ -1,7 +0,0 @@
-!function(){
-//! Constructorul User:
-function o(o,n){this.username=o,this.email=n}
-//! Definim metoda login pe prototipul User:
-//! Constructorul Admin:
-function n(n,e,t){o.call(this,n,e),this.title=t}o.prototype.login=function(){return console.log("".concat(this.username," has logged in.")),this},n.prototype=Object.create(o.prototype),n.prototype.deleteUser=function(){};var e=new o("danretegan","danretegan@yahoo.com"),t=new o("ion","ion@ion.com"),i=new n("admin","admin@admin.com","black-belt-ninja");console.log(e,t,i),e.login(),console.log(n.prototype)}();
-//# sourceMappingURL=index.bc5aa24c.js.map
